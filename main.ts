c3651fb2c087cc6a03a43018ea3d9acf23efa21f
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2022-01-13 17:26:47 +1300

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 function SetScore (miner1: Sprite, num: number) {
     if (num == 1) {
         CheckOre(miner1)
+    } else if (num == 2) {
+        CheckOre2(miner1)
+    } else if (num == 3) {
+        CheckOre3(miner1)
+    } else if (num == 4) {
+        CheckOre4(miner1)
     }
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -11,12 +17,32 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (controller.down.isPressed()) {
         if (tiles.tileIsWall(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom))) {
-            if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile`)) {
+            if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile1`)) {
+                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile2`)
+            } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile2`)) {
+                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile3`)
+            } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile3`)) {
+                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile5`)
+            } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile5`)) {
+                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile6`)
+            } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile6`)) {
+                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile7`)
+            } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile7`)) {
+                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile8`)
+            } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile8`)) {
+                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile9`)
+            } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile9`)) {
+                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile10`)
+            } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile10`)) {
+                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile11`)
+            } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile11`)) {
+                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile`)
+            } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile`)) {
                 tiles.setWallAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), false)
                 tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`cave background`)
             } else {
                 SetScore(miner, 1)
-                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile`)
+                tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Bottom), assets.tile`myTile1`)
             }
         }
     }
@@ -26,6 +52,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
                 tiles.setWallAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Top), false)
                 tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Top), assets.tile`cave background`)
             } else {
+                SetScore(miner, 2)
                 tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Top), assets.tile`myTile`)
             }
         }
@@ -36,6 +63,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
                 tiles.setWallAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Left), false)
                 tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Left), assets.tile`cave background`)
             } else {
+                SetScore(miner, 3)
                 tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Left), assets.tile`myTile`)
             }
         }
@@ -46,6 +74,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
                 tiles.setWallAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Right), false)
                 tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Right), assets.tile`cave background`)
             } else {
+                SetScore(miner, 4)
                 tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(miner), CollisionDirection.Right), assets.tile`myTile`)
             }
         }
@@ -73,6 +102,81 @@ function CheckOre (mySprite: Sprite) {
     } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Bottom), assets.tile`brown cristal ore0`)) {
         info.changeScoreBy(500)
     } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Bottom), assets.tile`brown cristal ore`)) {
+        info.changeScoreBy(1000)
+    }
+}
+function CheckOre4 (mySprite: Sprite) {
+    if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Right), assets.tile`white cristal ore0`)) {
+        info.changeScoreBy(1)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Right), assets.tile`black cristal ore0`)) {
+        info.changeScoreBy(5)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Right), assets.tile`blue cristal ore0`)) {
+        info.changeScoreBy(10)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Right), assets.tile`green cristal ore0`)) {
+        info.changeScoreBy(15)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Right), assets.tile`yellow cristal ore0`)) {
+        info.changeScoreBy(20)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Right), assets.tile`orange cristal ore0`)) {
+        info.changeScoreBy(30)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Right), assets.tile`red cristal ore0`)) {
+        info.changeScoreBy(50)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Right), assets.tile`purple cristal ore0`)) {
+        info.changeScoreBy(100)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Right), assets.tile`pink cristal ore0`)) {
+        info.changeScoreBy(150)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Right), assets.tile`brown cristal ore0`)) {
+        info.changeScoreBy(500)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Right), assets.tile`brown cristal ore`)) {
+        info.changeScoreBy(1000)
+    }
+}
+function CheckOre3 (mySprite: Sprite) {
+    if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Left), assets.tile`white cristal ore0`)) {
+        info.changeScoreBy(1)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Left), assets.tile`black cristal ore0`)) {
+        info.changeScoreBy(5)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Left), assets.tile`blue cristal ore0`)) {
+        info.changeScoreBy(10)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Left), assets.tile`green cristal ore0`)) {
+        info.changeScoreBy(15)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Left), assets.tile`yellow cristal ore0`)) {
+        info.changeScoreBy(20)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Left), assets.tile`orange cristal ore0`)) {
+        info.changeScoreBy(30)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Left), assets.tile`red cristal ore0`)) {
+        info.changeScoreBy(50)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Left), assets.tile`purple cristal ore0`)) {
+        info.changeScoreBy(100)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Left), assets.tile`pink cristal ore0`)) {
+        info.changeScoreBy(150)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Left), assets.tile`brown cristal ore0`)) {
+        info.changeScoreBy(500)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Left), assets.tile`brown cristal ore`)) {
+        info.changeScoreBy(1000)
+    }
+}
+function CheckOre2 (mySprite: Sprite) {
+    if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Top), assets.tile`white cristal ore0`)) {
+        info.changeScoreBy(1)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Top), assets.tile`black cristal ore0`)) {
+        info.changeScoreBy(5)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Top), assets.tile`blue cristal ore0`)) {
+        info.changeScoreBy(10)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Top), assets.tile`green cristal ore0`)) {
+        info.changeScoreBy(15)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Top), assets.tile`yellow cristal ore0`)) {
+        info.changeScoreBy(20)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Top), assets.tile`orange cristal ore0`)) {
+        info.changeScoreBy(30)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Top), assets.tile`red cristal ore0`)) {
+        info.changeScoreBy(50)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Top), assets.tile`purple cristal ore0`)) {
+        info.changeScoreBy(100)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Top), assets.tile`pink cristal ore0`)) {
+        info.changeScoreBy(150)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Top), assets.tile`brown cristal ore0`)) {
+        info.changeScoreBy(500)
+    } else if (tiles.tileAtLocationEquals(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Top), assets.tile`brown cristal ore`)) {
         info.changeScoreBy(1000)
     }
 }
